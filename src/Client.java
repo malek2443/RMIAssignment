5fb863a99b505ba5042e18ca83cbb660ef985253
malek2443 <95133428+malek2443@users.noreply.github.com>
2021-12-12 20:32:50 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,27 @@
 import java.rmi.Naming;
+import java.util.*;
 
 public class Client {
 
     public static void main(String[] args) {
         try {
            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+public static void main(String[] args) {
+try {
+MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+Scanner obj = new Scanner(System.in);
+String s = obj.nextLine();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+System.out.println(service.caseChanger(s));
+
+obj.close();
+
+} catch (Exception e) {
+e.printStackTrace();
+}
+}
 }
